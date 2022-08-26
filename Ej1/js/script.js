f7be173e4f07c6e3ec7051a323dfc6b656e6a0c2
dullkarlmarx <agustinvazquezrocha33@gmail.com>
2022-08-25 21:50:42 -0300

    Update script.js

--- Ej1/js/script.js
+++ Ej1/js/script.js
@@ -1,15 +1,36 @@
 // 1. Array de números:
 const numberArray = [4, 10, 34, 999, 0, -10000, -999, 58, 2, 4];
+
 // a. Muestra en consola el array ordenado de menor a mayor (ascendente)
+numberArray.sort((a, b) => {
+  return a - b;
+}
+)
+console.log(numberArray);
 
 // b. Muestra en consola el array ordenado de mayor a menor (descendente)
-
+numberArray.sort((a, b) => {
+  return b - a;
+}
+)
+console.log(numberArray);
 
 // 2. Array de String:
 const stringArray = ["a", "r", "alo", "www", "internet", "qqq", "bbb"];
+
 // a. Muestra en consola el array ordenado A-Z
 
+stringArray.sort();
+console.log(stringArray);
+
 // b. Muestra en consola el array ordenado Z-A
+
+stringArray.reverse();
+console.log(stringArray);
+
+
+// 3. Array de Number:
+
 
 
 // 3. Array de objetos:
@@ -22,4 +43,14 @@ const objectArray = [
 ];
 // a. Muestra en consola el array ordenado según el campo "age" de mayor a menor (descendente)
 
+objectArray.sort((a, b) => {
+  return b.age - a.age;
+});
+console.log(objectArray);
 // b. Muestra en consola el array ordenado según el campo "lastname" de A-Z
+
+objectArray.sort((a, b) => {
+  return a.lastname.localeCompare(b.lastname);
+}); 
+
+console.log(objectArray);
